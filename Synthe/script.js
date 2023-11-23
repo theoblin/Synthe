e8e49bf996a962f34b05e402f4a06ba70db3c5e5
Théo <46441220+theoblin@users.noreply.github.com>
2023-11-23 14:56:14 +0100

    reglages dans une modale

--- Synthe/script.js
+++ Synthe/script.js
@@ -1,15 +1,12 @@
-
-    
-    
-    function readURL(input) {
+     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
             reader.onload = function (e) {
                 $('#blah')
                     .attr('src', e.target.result)
-                    .width(150)
-                    .height(150);
+                    .width(195)
+                    .height(195);
             };
 
             $('.file-upload').hide();
@@ -17,3 +14,29 @@
         }
     }
  
+    $(document).ready(function() { 
+
+             // Get the modal
+     var modal = document.getElementById("myModal");
+
+     // Get the button that opens the modal
+     var btn = document.getElementById("myBtn");
+ 
+     // Get the <span> element that closes the modal
+     var span = document.getElementsByClassName("close")[0];
+ 
+     // When the user clicks on the button, open the modal
+     btn.onclick = function () {
+       modal.style.display = "block";
+     };
+ 
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function () {
+       modal.style.display = "none";
+     };
+    
+    });
+ 
+
+
+    
